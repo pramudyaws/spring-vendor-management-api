8f926bc53aa8e8eeeab1556f7ee5d60947628140
Pramudya Wibisono <pramudyaw.serious@gmail.com>
2024-11-06 21:50:09 +0700

    feat: implement CRUD vendor

---
+++ src/main/java/com/spring/vendorAPI/entities/Vendor.java
@@ -0,0 +1,22 @@
+package com.spring.vendorAPI.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "vendors")
+public class Vendor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+}
