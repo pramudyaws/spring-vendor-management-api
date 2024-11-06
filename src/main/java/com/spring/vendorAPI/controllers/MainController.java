06e8f2ad8e78b37535b780a610f07c770f5f8dfc
Pramudya Wibisono <pramudyaw.serious@gmail.com>
2024-11-06 21:17:36 +0700

    feat: create welcome (index) API

---
+++ src/main/java/com/spring/vendorAPI/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.spring.vendorAPI.controllers;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public Map<String, String> helloWorld() {
+        return Collections.singletonMap("message", "Welcome to Spring Vendor Management API!");
+    }
+}
