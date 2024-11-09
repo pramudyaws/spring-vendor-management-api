01abec331bea5332edc748f36429b656c90ee563
Pramudya Wibisono <pramudyaw.serious@gmail.com>
2024-11-09 16:59:08 +0700

    chore: add example in vendor request dtos

--- src/main/java/com/spring/vendorAPI/dtos/UpdateVendorDto.java
+++ src/main/java/com/spring/vendorAPI/dtos/UpdateVendorDto.java
@@ -1,10 +1,12 @@
 package com.spring.vendorAPI.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public class UpdateVendorDto {
 
     @NotBlank(message = "Vendor name is required")
+    @Schema(description = "Vendor name", example = "Vendor 1 updated")
     private String name;
 
     public String getName() {
